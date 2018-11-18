d3accc410d4f59c3e9fcdd1f5e166d2281a1d34b
Kunal Mehta <knlmht2@gmail.com>
2018-11-18 18:16:09 +0530

    Updates

---
+++ precache-manifest.fed0b5eb63fdc318a3c4243eddf562cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "313984ebef1cd36a350e",
+    "url": "/kunal-mehta/stocks-dashboard/static/js/runtime~main.313984eb.js"
+  },
+  {
+    "revision": "3d19e6f331c983ef1040",
+    "url": "/kunal-mehta/stocks-dashboard/static/js/main.3d19e6f3.chunk.js"
+  },
+  {
+    "revision": "d854135a31db8626481e",
+    "url": "/kunal-mehta/stocks-dashboard/static/js/1.d854135a.chunk.js"
+  },
+  {
+    "revision": "3d19e6f331c983ef1040",
+    "url": "/kunal-mehta/stocks-dashboard/static/css/main.347b1211.chunk.css"
+  },
+  {
+    "revision": "ce313dca6940077c58993adb1082d5e3",
+    "url": "/kunal-mehta/stocks-dashboard/index.html"
+  }
+];
